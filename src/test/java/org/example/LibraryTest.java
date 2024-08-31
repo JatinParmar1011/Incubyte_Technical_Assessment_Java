7aa7134321deac39f70578968acd98a469913e57
JatinParmar1011 <jatinparmar1014@gmail.com>
2024-08-31 14:26:35 +0530

    Add test

--- src/test/java/org/example/LibraryTest.java
+++ src/test/java/org/example/LibraryTest.java
@@ -1,7 +1,14 @@
 package org.example;
 
+import org.junit.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class LibraryTest {
+    @Test
+    public void shouldAnswerIsTrue()
+    {
+        assertTrue( true );
+    }
 
 }
