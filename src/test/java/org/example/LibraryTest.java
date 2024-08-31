8590e2e306b348c1b99b737a4c64bb20b484831d
JatinParmar1011 <jatinparmar1014@gmail.com>
2024-08-31 15:47:09 +0530

    [RED] Test Case for Adding Valid Book

--- src/test/java/org/example/LibraryTest.java
+++ src/test/java/org/example/LibraryTest.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class LibraryTest {
     Library library = new Library();
 
-    // Test the addition of a valid Book to the library
+    // Test the addition of a valid Book
     public void testAddBook() {
         Book validBook = new Book("9788173711466", "Wings of Fire", "A.P.J. Abdul Kalam", 1999);
         library.addBook(validBook);
