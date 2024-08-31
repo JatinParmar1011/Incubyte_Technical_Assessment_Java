ed5e446141d6ec97764007cee146f21ffea2ce8c
JatinParmar1011 <jatinparmar1014@gmail.com>
2024-08-31 16:22:57 +0530

    [RED] Added Test Case for Handling Null Values in Add Book

--- src/test/java/org/example/LibraryTest.java
+++ src/test/java/org/example/LibraryTest.java
@@ -6,9 +6,14 @@ import static org.junit.jupiter.api.Assertions.*;
 class LibraryTest {
     Library library = new Library();
 
-    // Test the addition of a valid Book
+    @Test
     public void testAddBook() {
+        // 1. Test case for addition of a valid Book
         Book validBook = new Book("9788173711466", "Wings of Fire", "A.P.J. Abdul Kalam", 1999);
         library.addBook(validBook);
+
+        // 2. Test case for adding a null book
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> library.addBook(null));
+        assertEquals("Book should not be null", exception.getMessage());
     }
 }
