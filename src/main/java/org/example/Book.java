142103875c13ae03ae9200a2416e83aa6e160b6e
JatinParmar1011 <jatinparmar1014@gmail.com>
2024-08-31 15:34:52 +0530

    [Enhance] Added Validation Logic for Book Class

--- src/main/java/org/example/Book.java
+++ src/main/java/org/example/Book.java
@@ -7,9 +7,26 @@ public class Book {
     private final int publicationYear;
 
     public Book(String isbn, String title, String author, int publicationYear) {
+        validateMandatoryFields(isbn, title, author, publicationYear);
+
         this.isbn = isbn;
         this.title = title;
         this.author = author;
         this.publicationYear = publicationYear;
+    }
+
+    private void validateText(String value, String message) {
+        if (value == null || value.trim().isEmpty()) {
+            throw new IllegalArgumentException(message);
+        }
+    }
+
+    private void validateMandatoryFields(String isbn, String title, String author, int publicationYear) {
+        validateText(isbn, "ISBN should not be null or empty");
+        validateText(title, "Title should not be null or empty");
+        validateText(author, "Author should not be null or empty");
+        if (publicationYear <= 0) {
+            throw new IllegalArgumentException("Publication year should be a positive integer");
+        }
     }
 }
